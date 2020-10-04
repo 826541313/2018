2cd6fda2a22936233027999f9ee651b2ef2fdaa6
郝天恒 <45160592+826541313@users.noreply.github.com>
2020-10-04 17:04:58 +0800

    Add files via upload

---
+++ precache-manifest.aef509f5905a567c34634e82f102b2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660af21fe566ae905148aae973b9a61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9487709e8e36c965036",
+    "url": "/static/js/main.12d2f0a1.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
